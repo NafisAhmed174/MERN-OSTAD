ec0f41c5a80322beee6752c718d7ac2956b50768
NafisAhmed174 <nafisahmed947@gmail.com>
2024-10-17 18:29:59 +0600

    added controller for read and update profile

--- Module 21/controllers/authController.js
+++ Module 21/controllers/authController.js
@@ -23,5 +23,16 @@ exports.login = async (req, res) => {
     const token = jwt.sign({ id: student._id }, 'your_jwt_secret', { expiresIn: '1h' });
     res.cookie('token', token, { httpOnly: true });
     res.status(200).json({ message: 'Login successful', token });
-  };
+};
+
+exports.getProfile = async (req, res) => {
+    const student = await Student.findById(req.studentId);
+    if(!student) return res.status(404).json({ error: 'Student not found' });
+    res.status(200).json(student);
+};
+  
+exports.updateProfile = async (req, res) => {
+    const student = await Student.findByIdAndUpdate(req.studentId, req.body, { new: true });
+    res.status(200).json({ message: 'Profile updated', student });
+};
   
