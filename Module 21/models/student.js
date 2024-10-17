d60986cc356a9b1d92d195746e0e1f5c01bce3a0
NafisAhmed174 <nafisahmed947@gmail.com>
2024-10-17 18:23:39 +0600

    created module 21 and all the files and student model

---
+++ Module 21/models/student.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const studentSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    profilePic: { type: String }
+});
+
+// Hash password before saving
+studentSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+module.exports = mongoose.model('Student', studentSchema);
